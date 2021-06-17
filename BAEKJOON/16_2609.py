fecf6ef0ecb6d1e89d7aec76728dac4179f43510
sanghyun jang <firstquarter88@gmail.com>
2021-06-18 02:26:08 +0900

    2021-06-18

---
+++ BAEKJOON/16_2609.py
@@ -0,0 +1,22 @@
+
+
+# 최소공배수(LCM : Least Common Multiple
+
+# 최대공약수(GCD : Greatest Common Divisor)
+# a를 b로 나눈 나머지를 r
+# a와 b의 최대공약수는 b 와 r 의 최대공약수
+# 즉 a와 b의 최대공약수는 b와 a%b 의 최대공약수
+
+a = 24
+b = 18
+
+
+
+
+
+
+
+# import math # 사기...
+# a, b = map(int, input().split())
+# print(math.gcd(a, b))
+# print(math.lcm(a, b))
