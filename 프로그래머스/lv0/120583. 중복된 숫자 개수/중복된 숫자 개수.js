2e4a13759d264d5f917a1cdfa61995b2b4ac1a7c
SangHyun Jang <81303869+firstquarter-J@users.noreply.github.com>
2023-02-18 00:55:46 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.04 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -1,7 +1,8 @@
 function solution(array, n) {
     let answer = 0;
-    for ( let i = 0; i < array.length; i++ ) {
-        if (array[i] === n) answer += 1;
-    }
-    return answer;
+    // for ( let i = 0; i < array.length; i++ ) {
+    //     if (array[i] === n) answer += 1;
+    // }
+    // return answer;
+    return array.filter((arr) => arr === n).length;
 }
