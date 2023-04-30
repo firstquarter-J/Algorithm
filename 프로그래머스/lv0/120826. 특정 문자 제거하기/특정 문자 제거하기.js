90df845c07453cc46c0aed984b98ae6f6b7066d5
SangHyun Jang <81303869+firstquarter-J@users.noreply.github.com>
2023-05-01 02:58:13 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,3 +1,4 @@
 function solution(my_string, letter) {
-    return my_string.replace(new RegExp(letter, 'g'), '')
+    // return my_string.replace(new RegExp(letter, 'g'), '')
+    return my_string.replaceAll(letter, '')
 }
