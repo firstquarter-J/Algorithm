1a10eb06a551d855c847c2489358217a309e8612
SangHyun Jang <81303869+firstquarter-J@users.noreply.github.com>
2023-07-03 15:23:32 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,19 +1,31 @@
-function gcd(a, b) {
-    return b === 0 ? a : gcd(b, a % b);
+// function gcd(a, b) {
+//     return b === 0 ? a : gcd(b, a % b);
+// }
+
+// function solution(numer1, denom1, numer2, denom2) {
+    
+//     const lcm = (denom1 * denom2) / gcd(denom1, denom2);
+    
+//     const newNumer1 = numer1 * (lcm / denom1);
+//     const newNumer2 = numer2 * (lcm / denom2);
+    
+//     const resultNumer = newNumer1 + newNumer2;
+    
+//     const commonDivisor = gcd(resultNumer, lcm);
+//     const reducedNumer = resultNumer / commonDivisor;
+//     const reducedDenom = lcm / commonDivisor;
+    
+//     return [reducedNumer, reducedDenom];
+// }
+
+function fnGCD(a, b){
+    return (a%b) ? fnGCD(b, a%b) : b;
 }
 
-function solution(numer1, denom1, numer2, denom2) {
-    
-    const lcm = (denom1 * denom2) / gcd(denom1, denom2);
-    
-    const newNumer1 = numer1 * (lcm / denom1);
-    const newNumer2 = numer2 * (lcm / denom2);
-    
-    const resultNumer = newNumer1 + newNumer2;
-    
-    const commonDivisor = gcd(resultNumer, lcm);
-    const reducedNumer = resultNumer / commonDivisor;
-    const reducedDenom = lcm / commonDivisor;
-    
-    return [reducedNumer, reducedDenom];
+function solution(denum1, num1, denum2, num2) {
+    let denum = denum1*num2 + denum2*num1;
+    let num = num1 * num2;
+    let gcd = fnGCD(denum, num); //최대공약수
+
+    return [denum/gcd, num/gcd];
 }
