bc6a1df6f4d3fad777ed0cbaae31a68d93356fb2
SangHyun Jang <81303869+firstquarter-J@users.noreply.github.com>
2023-07-06 13:19:01 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,17 +1,24 @@
-function solution(array) {
-    // 배열 삽입 정렬
-    for ( let i = 1; i < array.length; i++ ) {
-        const checkValue = array[i];
-        let left = i - 1;
+// function solution(array) {
+//     // 배열 삽입 정렬
+//     for ( let i = 1; i < array.length; i++ ) {
+//         const checkValue = array[i];
+//         let left = i - 1;
         
-        while ( left >= 0 && array[left] > checkValue ) {
-            array[left+1]  = array[left];
-            left--;
-        }
-        array[left+1] = checkValue;
-    }
+//         while ( left >= 0 && array[left] > checkValue ) {
+//             array[left+1]  = array[left];
+//             left--;
+//         }
+//         array[left+1] = checkValue;
+//     }
 
-    // 배열 길이 / 2 (Math.floor 로 나머지 버림)
-    const center = Math.floor(array.length / 2)
-    return array[center];
+//     // 배열 길이 / 2 (Math.floor 로 나머지 버림)
+//     const center = Math.floor(array.length / 2)
+//     return array[center];
+// }
+
+function solution(array) {
+    // 배열 정렬 (sort 함수)
+    const sortedArr = array.sort((a, b) => a - b);
+    const center = Math.floor(sortedArr.length / 2)
+    return sortedArr[center]
 }
