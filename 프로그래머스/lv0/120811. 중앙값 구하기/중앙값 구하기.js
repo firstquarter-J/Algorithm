0a8ee763a9678f06d958bb30fca56c8fb24bab43
SangHyun Jang <81303869+firstquarter-J@users.noreply.github.com>
2023-07-06 13:04:51 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -0,0 +1,17 @@
+function solution(array) {
+    // 배열 삽입 정렬
+    for ( let i = 1; i < array.length; i++ ) {
+        const checkValue = array[i];
+        let left = i - 1;
+        
+        while ( left >= 0 && array[left] > checkValue ) {
+            array[left+1]  = array[left];
+            left--;
+        }
+        array[left+1] = checkValue;
+    }
+
+    // 배열 길이 / 2 (Math.floor 로 나머지 버림)
+    const center = Math.floor(array.length / 2)
+    return array[center];
+}
