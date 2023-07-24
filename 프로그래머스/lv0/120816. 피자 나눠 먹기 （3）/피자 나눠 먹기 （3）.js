d0cc9d5617dbd1e7d60b860c0f55316fa082e320
SangHyun Jang <81303869+firstquarter-J@users.noreply.github.com>
2023-07-25 03:51:57 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
@@ -1,3 +1,10 @@
 function solution(slice, n) {
-    return Math.ceil(n/slice);
+    // return Math.ceil(n/slice);
+    let pizzaCount = 1;
+    
+    while ( pizzaCount * slice < n ) {
+        pizzaCount++
+    }
+    
+    return pizzaCount
 }
