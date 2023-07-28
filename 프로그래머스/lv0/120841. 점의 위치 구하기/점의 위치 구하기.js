fb98c7030de1af325eef4462bee28b3fab0b570c
SangHyun Jang <81303869+firstquarter-J@users.noreply.github.com>
2023-07-28 19:44:35 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -1,18 +1,22 @@
+// function solution(dot) {
+//     let answer = 0;
+    
+//     if ( dot[0] > 0 && dot[1] > 0 ) {
+//         answer = 1;
+//     }
+//     if ( dot[0] < 0 && dot[1] > 0 ) {
+//         answer = 2;
+//     }
+//     if ( dot[0] < 0 && dot[1] < 0 ) {
+//         answer = 3;
+//     }
+//     if ( dot[0] > 0 && dot[1] < 0 ) {
+//         answer = 4;
+//     }
+    
+//     return answer;
+// }
+
 function solution(dot) {
-    let answer = 0;
-    
-    if ( dot[0] > 0 && dot[1] > 0 ) {
-        answer = 1;
-    }
-    if ( dot[0] < 0 && dot[1] > 0 ) {
-        answer = 2;
-    }
-    if ( dot[0] < 0 && dot[1] < 0 ) {
-        answer = 3;
-    }
-    if ( dot[0] > 0 && dot[1] < 0 ) {
-        answer = 4;
-    }
-    
-    return answer;
+    return dot[0] > 0 ? dot[1] > 0 ? 1 : 4 : dot[1] < 0 ? 3 : 2;
 }
