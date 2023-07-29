1d2ae647575f198e1eace8ba2430fbfe50abd52d
SangHyun Jang <81303869+firstquarter-J@users.noreply.github.com>
2023-07-30 01:46:45 +0900

    [level 0] Title: 모음 제거, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -1,3 +1,21 @@
+// function solution(my_string) {
+//     return my_string.replace(/[aeiou]/g, '');
+// }
+
+// function solution(my_string) {
+//     let answer = '';
+//     const check = ['a', 'e', 'i', 'o', 'u'];
+    
+//     for (let i in my_string) {
+//         if (!check.includes(my_string[i])) answer += my_string[i];
+//     }
+    
+//     return answer;
+// }
+
 function solution(my_string) {
-    return my_string.replace(/[aeiou]/g, '');
+    return [...my_string]
+        .filter((v) => !['a', 'e', 'i', 'o', 'u'].includes(v))
+        .join('');
+
 }
