c709f65ea1ac4cd2c8277906f820c38624d263ee
SangHyun Jang <81303869+firstquarter-J@users.noreply.github.com>
2023-08-02 00:41:54 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.08 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,13 +1,17 @@
-function solution(my_string) {
-    var answer = [];
+// function solution(my_string) {
+//     var answer = [];
     
-    arr = [...my_string];
+//     arr = [...my_string];
     
-    for (let i in arr) {
-        if (!isNaN(arr[i])) {
-            answer.push(Number(arr[i]))
-            }
-    }
+//     for (let i in arr) {
+//         if (!isNaN(arr[i])) {
+//             answer.push(Number(arr[i]))
+//             }
+//     }
         
-    return answer.sort();
+//     return answer.sort();
+// }
+
+function solution(my_string) {
+    return [...my_string].filter((v) => !isNaN(v)).map((v) => Number(v)).sort();
 }
