b876a966d0914c0982b55d0eed5e7d8258de2cfe
SangHyun Jang <81303869+firstquarter-J@users.noreply.github.com>
2023-08-03 19:13:06 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 2.62 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,17 +1,36 @@
-// function solution(my_string) {
-//     var answer = [];
-    
-//     arr = [...my_string];
-    
-//     for (let i in arr) {
-//         if (!isNaN(arr[i])) {
-//             answer.push(Number(arr[i]))
-//             }
-//     }
-        
-//     return answer.sort();
-// }
-
 function solution(my_string) {
-    return [...my_string].filter((v) => !isNaN(v)).map((v) => Number(v)).sort();
+    var answer = [];
+    
+    arr = [...my_string];
+    
+    for (let i in arr) {
+        if (!isNaN(arr[i])) {
+            answer.push(Number(arr[i]))
+            }
+    }
+    
+    answer = insertionSort(answer);
+        
+    return answer;
 }
+
+function insertionSort (array) {
+    for (let i = 1; i < array.length; i++) {
+        let cur = array[i];
+        let left = i - 1;
+
+        while (left >= 0 && array[left] > cur) {
+            array[left + 1] = array[left];
+            left--;
+        }
+
+        array[left + 1] = cur;
+        console.log(`${i}회전: ${array}`);
+        }
+    
+    return array;
+}
+
+// function solution(my_string) {
+//     return [...my_string].filter((v) => !isNaN(v)).map((v) => Number(v)).sort();
+// }
