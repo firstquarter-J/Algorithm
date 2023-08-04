9efa86b0c5800355a78164ee9e18764a3de42477
SangHyun Jang <81303869+firstquarter-J@users.noreply.github.com>
2023-08-05 01:52:47 +0900

    [level 0] Title: 세균 증식, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.js
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.js
@@ -1,8 +1,13 @@
-function solution(n, t) {
-    let answer = n;
+// function solution(n, t) {
+//     let answer = n;
     
-    for (let i = 1; i <= t; i++) {
-        answer = answer * 2
-    }
-    return answer;
+//     for (let i = 1; i <= t; i++) {
+//         answer = answer * 2
+//     }
+//     return answer;
+// }
+
+function solution(n, t) {
+    while (t-- > 0) n *=  2
+    return n;
 }
