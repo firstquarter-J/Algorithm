cbb3ae4029c63f1d8fcb79db9df28886ea625865
SangHyun Jang <81303869+firstquarter-J@users.noreply.github.com>
2023-08-06 02:38:48 +0900

    [level 0] Title: 가위 바위 보, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,12 +1,22 @@
-function solution(rsp) {
-    let answer = '';
-    const rspArr = [...rsp];
+// function solution(rsp) {
+//     let answer = '';
+//     const rspArr = [...rsp];
     
-    for (let i in rspArr) {
-        if (rspArr[i] === '2') answer += '0'
-        if (rspArr[i] === '0') answer += '5'
-        if (rspArr[i] === '5') answer += '2'
+//     for (let i in rspArr) {
+//         if (rspArr[i] === '2') answer += '0'
+//         if (rspArr[i] === '0') answer += '5'
+//         if (rspArr[i] === '5') answer += '2'
+//     }
+    
+//     return answer;
+// }
+
+function solution(rsp) {
+    const rspObject = {
+        2: 0,
+        0: 5,
+        5: 2
     }
     
-    return answer;
+    return [...rsp].map(v => rspObject[v]).join('')
 }
