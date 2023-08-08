93cfd843807da38112c952cdaebf8b4aa90ca376
SangHyun Jang <81303869+firstquarter-J@users.noreply.github.com>
2023-08-08 14:07:37 +0900

    [level 0] Title: 암호 해독, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
@@ -1,9 +1,13 @@
-function solution(cipher, code) {
-    let answer = '';
+// function solution(cipher, code) {
+//     let answer = '';
     
-    for (let i = code - 1; i < cipher.length; i += code) {
-            answer += cipher[i];
-    }
+//     for (let i = code - 1; i < cipher.length; i += code) {
+//             answer += cipher[i];
+//     }
 
-    return answer;
+//     return answer;
+// }
+
+function solution(cipher, code) {
+    return [...cipher].filter((_, index) => (index + 1) % code === 0).join('');
 }
