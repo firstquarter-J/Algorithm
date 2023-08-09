5dc351573d2112826be685c21f1d39036bca1e74
SangHyun Jang <81303869+firstquarter-J@users.noreply.github.com>
2023-08-09 22:29:16 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.55 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,14 +1,22 @@
+// function solution(my_string) {
+//     let answer = '';
+//     const strArr = [...my_string];
+    
+//     for (let i in strArr) {
+//         if (strArr[i] === strArr[i].toUpperCase()) {
+//             answer += strArr[i].toLowerCase();        
+//         } else {
+//             answer += strArr[i].toUpperCase();
+//         }
+//     }
+    
+//     return answer;
+// }
+
 function solution(my_string) {
     let answer = '';
-    const strArr = [...my_string];
     
-    for (let i in strArr) {
-        if (strArr[i] === strArr[i].toUpperCase()) {
-            answer += strArr[i].toLowerCase();        
-        } else {
-            answer += strArr[i].toUpperCase();
-        }
-    }
+    for (let i of my_string) answer += i === i.toUpperCase() ? i.toLowerCase() : i.toUpperCase();
     
     return answer;
 }
