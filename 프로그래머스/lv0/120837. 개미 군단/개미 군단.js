9bd07680d0a166a85d376a74d453100dff9658b0
SangHyun Jang <81303869+firstquarter-J@users.noreply.github.com>
2023-08-12 05:22:36 +0900

    [level 0] Title: 개미 군단, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,6 +1,14 @@
+// function solution(hp) {
+//     fiveCount = Math.floor(hp / 5);
+//     threeCount = Math.floor((hp - fiveCount * 5) / 3);
+//     oneCount = hp - (fiveCount * 5) - (threeCount * 3);
+
+//     return fiveCount + threeCount + oneCount;
+// }
+
 function solution(hp) {
-    fiveCount = Math.floor(hp / 5);
-    threeCount = Math.floor((hp - fiveCount * 5) / 3);
+    fiveCount = ~~(hp / 5);
+    threeCount = ~~((hp - fiveCount * 5) / 3);
     oneCount = hp - (fiveCount * 5) - (threeCount * 3);
 
     return fiveCount + threeCount + oneCount;
