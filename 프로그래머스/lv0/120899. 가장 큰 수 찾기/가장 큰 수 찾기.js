cdd0454223ac81104f2de7ed757088ad4aef046c
SangHyun Jang <81303869+firstquarter-J@users.noreply.github.com>
2023-08-16 21:01:48 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,17 +1,19 @@
+// function solution(array) {
+//     let answer = [];
+//     let checkValue = array[0];
+    
+//     for (let i = 0; i < array.length; i++) {
+//         if (checkValue < array[i]) checkValue = array[i];
+//     }
+//     answer.push(checkValue)
+    
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] === checkValue) answer.push(i)
+//     }
+
+//     return answer;
+// }
+
 function solution(array) {
-    let answer = [];
-    let checkValue = array[0];
-    
-    for (let i = 0; i < array.length; i++) {
-        if (checkValue < array[i]) checkValue = array[i];
-    }
-    answer.push(checkValue)
-    
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === checkValue) answer.push(i)
-    }
-    
-    
-    
-    return answer;
+    return [Math.max(...array), array.indexOf(Math.max(...array))]
 }
