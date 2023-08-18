07cf4509d9bf41f6b04ba6845222879c758592f8
SangHyun Jang <81303869+firstquarter-J@users.noreply.github.com>
2023-08-18 22:44:51 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,8 +1,20 @@
+// function solution(numbers) {
+//     numbers.sort((a, b) => a - b);
+    
+//     minIndexMul = numbers[0] * numbers[1];
+//     maxIndexMul = numbers[numbers.length - 1] * numbers[numbers.length - 2];
+    
+//     return minIndexMul > maxIndexMul ? minIndexMul : maxIndexMul;
+// }
+
 function solution(numbers) {
-    numbers.sort((a, b) => a - b);
+    let answer = [];
     
-    minIndexMul = numbers[0] * numbers[1];
-    maxIndexMul = numbers[numbers.length - 1] * numbers[numbers.length - 2];
-    
-    return minIndexMul > maxIndexMul ? minIndexMul : maxIndexMul;
+    for (let i = 0; i < numbers.length - 1; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            answer.push(numbers[i] * numbers[j]);
+        }
+    }
+
+    return Math.max(...answer);
 }
