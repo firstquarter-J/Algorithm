928c80647c29277752e89baabb28647925d1e0fc
SangHyun Jang <81303869+firstquarter-J@users.noreply.github.com>
2023-08-19 13:48:51 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,9 +1,12 @@
-function solution(numbers, direction) {
-    if (direction === 'right') {
-        numbers.unshift(numbers.pop());
-    } else {
-        numbers.push(numbers.shift());
-    }
+// function solution(numbers, direction) {
+//     direction === 'right' ? numbers.unshift(numbers.pop()) : numbers.push(numbers.shift());
     
-    return numbers;
+//     return numbers;
+// }
+
+
+function solution(numbers, direction) {
+    return direction === 'right' 
+        ? [numbers[numbers.length - 1], ...numbers.slice(0, numbers.length - 1)]
+        : [...numbers.slice(1), numbers[0]];
 }
