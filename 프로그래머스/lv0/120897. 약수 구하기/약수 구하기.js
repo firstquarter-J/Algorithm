72c3178840d5af6a1897ea2b8f4d23921c80556f
SangHyun Jang <81303869+firstquarter-J@users.noreply.github.com>
2023-08-21 08:43:05 +0900

    [level 0] Title: 약수 구하기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
@@ -1,11 +1,18 @@
+// function solution(n) {
+//     let answer = [];
+    
+//     for (let i = 1; i <= n; i++) {
+//         if (n % i === 0) {
+//             answer.push(i)
+//         }
+//     }
+    
+//     return answer;
+// }
+
 function solution(n) {
-    let answer = [];
-    
-    for (let i = 1; i <= n; i++) {
-        if (n % i === 0) {
-            answer.push(i)
-        }
-    }
-    
-    return answer;
+    return Array(n)
+            .fill(0)
+            .map((v, index) => v + index + 1)
+            .filter((v) => n % v === 0);
 }
