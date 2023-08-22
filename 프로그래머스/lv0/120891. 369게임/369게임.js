3b3307575e0f6c00c3a846de3214f2fd29dcc5d8
SangHyun Jang <81303869+firstquarter-J@users.noreply.github.com>
2023-08-22 20:48:38 +0900

    [level 0] Title: 369게임, Time: 3.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,10 +1,17 @@
+// function solution(order) {
+//     let answer = 0;
+//     const orderArr = order.toString().split('');
+    
+//     for (let i of orderArr) {
+//         if (i !== '0' && Number(i) % 3 === 0) answer += 1;
+//     }
+    
+//     return answer;
+// }
+
 function solution(order) {
-    let answer = 0;
-    const orderArr = order.toString().split('');
-    
-    for (let i of orderArr) {
-        if (i !== '0' && Number(i) % 3 === 0) answer += 1;
-    }
-    
-    return answer;
+    console.log((order + '').split(/[369]/))
+    return (order + '')
+            .split(/[369]/)
+            .length - 1;
 }
