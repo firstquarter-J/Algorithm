2843f64276d140221bdf1a8404a72aed482905ec
SangHyun Jang <81303869+firstquarter-J@users.noreply.github.com>
2023-09-04 16:22:50 +0900

    [level 0] Title: 숫자 찾기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
@@ -1,12 +1,16 @@
-function solution(num, k) {
-    let answer = -1;
-    const numArr = num.toString().split('');
+// function solution(num, k) {
+//     let answer = -1;
+//     const numArr = num.toString().split('');
     
-    for (let i = 0; i < numArr.length; i++) {
-        if (numArr[i] === String(k)) {
-            answer = i + 1;
-            break;
-        }
-    }
-    return answer;
+//     for (let i = 0; i < numArr.length; i++) {
+//         if (numArr[i] === String(k)) {
+//             answer = i + 1;
+//             break;
+//         }
+//     }
+//     return answer;
+// }
+
+function solution(num, k) {
+    return num.toString().split('').map((v) => Number(v)).indexOf(k) + 1 || -1;
 }
