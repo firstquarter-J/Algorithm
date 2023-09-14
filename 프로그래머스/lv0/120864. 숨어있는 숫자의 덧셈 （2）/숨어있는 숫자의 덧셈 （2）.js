0bd9b70f65ea99c71d267baf54d8c89be6c945eb
SangHyun Jang <81303869+firstquarter-J@users.noreply.github.com>
2023-09-14 22:16:37 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.21 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,3 +1,4 @@
 function solution(my_string) {
-    return my_string.match(/\d+/g) ? my_string.match(/\d+/g).reduce((a, c) => a + Number(c), 0) : 0
+    return my_string.split(/\D+/).reduce((a, c) => a + Number(c), 0);
+    // return my_string.match(/\d+/g) ? my_string.match(/\d+/g).reduce((a, c) => a + Number(c), 0) : 0
 }
