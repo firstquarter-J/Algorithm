be4d9483e43eaaeeb7cc32df93cfbb327a2b5ce4
SangHyun Jang <81303869+firstquarter-J@users.noreply.github.com>
2023-09-18 22:31:35 +0900

    [level 0] Title: k의 개수, Time: 7.93 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.js
@@ -1,7 +1,16 @@
+// function solution(i, j, k) {
+//     let sum = '';
+//     for (let x = i; x <= j; x++) {
+//         sum += String(x);
+//     }
+//     return sum.split(k).length - 1;
+// }
+
 function solution(i, j, k) {
     let sum = '';
-    for (let x = i; x <= j; x++) {
-        sum += String(x);
+    for (i; i<= j; i++) {
+        sum += String(i);
     }
+    
     return sum.split(k).length - 1;
 }
