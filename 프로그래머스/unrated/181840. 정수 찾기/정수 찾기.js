8b18d2b559c458e8c976895e9412797f3289cf5c
SangHyun Jang <81303869+firstquarter-J@users.noreply.github.com>
2023-09-27 11:00:25 +0900

    [unrated] Title: 정수 찾기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.js
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.js
@@ -1,3 +1,9 @@
-function solution(num_list, n) {
-    return num_list.indexOf(n) === -1 ? 0 : 1;
-}
+// function solution(num_list, n) {
+//     return num_list.indexOf(n) === -1 ? 0 : 1;
+// }
+
+// function solution(num_list, n) {
+//     return num_list.includes(n) ? 1 : 0;
+// }
+
+const solution = (num_list, n) => num_list.includes(n) ? 1 : 0;
