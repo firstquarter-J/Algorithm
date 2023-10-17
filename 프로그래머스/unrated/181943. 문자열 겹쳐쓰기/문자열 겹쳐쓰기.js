067a104df792cdbeac9db790b4652a8d824da954
SangHyun Jang <81303869+firstquarter-J@users.noreply.github.com>
2023-10-18 02:12:12 +0900

    [level unrated] Title: 문자열 겹쳐쓰기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -1,5 +1,9 @@
+// function solution(my_string, overwrite_string, s) {
+//     const str = [...my_string];
+//     str.splice(s, overwrite_string.length, overwrite_string);
+//     return str.join('');
+// }
+
 function solution(my_string, overwrite_string, s) {
-    const str = [...my_string];
-    str.splice(s, overwrite_string.length, overwrite_string);
-    return str.join('');
+    return my_string.slice(0, s) + overwrite_string + my_string.slice(s + overwrite_string.length);
 }
